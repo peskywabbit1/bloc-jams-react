fa31725569f0ee7b9987f1fd07bbb780d5563e2f
Jessica Walker <peskywabbit@hotmail.com>
2019-04-18 11:43:28 -0700

    all pages updated. For Michael Davis review 2

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
         <Route exact path="/" component={Landing} />
         <Route path="/library" component={Library} />
         <Route path="/album/:slug" component={Album} />
+        <Route path="/album/:song" component={Album} />
       </main>
       </div>
     );
